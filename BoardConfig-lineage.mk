6db765bf5a5bba3bdb1acf5b1cb3a3378f789ebf
Rashed Abdel-Tawab <rashed@linux.com>
2019-09-06 19:56:16 -0700

    bonito: Define paths to DTC and MKDTIMG
    These are no longer on PATH, so we have to define them explicitly
    for the kernel make system to find them.
    
    Change-Id: I69f1a01f2f3d4c90e5a2337f44ee8e1bcb1cc1e8

--- BoardConfig-lineage.mk
+++ BoardConfig-lineage.mk
@@ -25,3 +25,6 @@ TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_CONFIG := shag_b4s4_defconfig
 TARGET_KERNEL_SOURCE := kernel/google/msm-4.9
+TARGET_KERNEL_ADDITIONAL_FLAGS := \
+    DTC=$(shell pwd)/prebuilts/misc/$(HOST_OS)-x86/dtc/dtc \
+    MKDTIMG=$(shell pwd)/prebuilts/misc/$(HOST_OS)-x86/libufdt/mkdtimg
