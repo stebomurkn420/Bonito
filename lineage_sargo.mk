5532493e4f8d37a36afd39d20ae481a1d749935d
shagbag913 <sh4gbag913@gmail.com>
2020-01-04 15:54:22 -0500

    Initial Lineage product bring-up
    Change-Id: If5a3054f55965fa9600039d19a4c812326104575

---
+++ lineage_sargo.mk
@@ -0,0 +1,16 @@
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2220
+TARGET_SCREEN_WIDTH := 1080
+
+# Inherit common Lineage product configuration
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/google/bonito/aosp_sargo.mk)
+
+PRODUCT_NAME := lineage_sargo
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Pixel 3a
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=sargo
